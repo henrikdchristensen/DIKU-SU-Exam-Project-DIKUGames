300a2fb398b0614138c6be91ccef3375b960f040
Henrik Daniel Christensen <djl587@alumni.ku.dk>
2022-02-26 15:13:39 +0100

    5: add a player to the game

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,25 +1,30 @@
-using DIKUArcade;
-using DIKUArcade.GUI;
-using DIKUArcade.Input;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga
 {
-    public class Game : DIKUGame
+    public class Game
     {
-        public Game(WindowArgs windowArgs) : base(windowArgs) {
-            // TODO: Set key event handler (inherited window field of DIKUGame class)
+        private Player player;
+
+        public Game() {
+            player = new Player(
+            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("Assets", "Images", "Player.png")));
         }
 
         //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
 
-        public override void Render()
-        {
-            throw new System.NotImplementedException("Galaga game has nothing to render yet.");
-        }
+        // public override void Render()
+        // {
+        //     throw new System.NotImplementedException("Galaga game has nothing to render yet.");
+        // }
 
-        public override void Update()
-        {
-            throw new System.NotImplementedException("Galaga game has no entities to update yet.");
-        }
+        // public override void Update()
+        // {
+        //     throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+        // }
     }
 }
