90c901db79feb0e4cd8287ff427b3c9bb5bb79f6
Henrik Daniel Christensen <djl587@alumni.ku.dk>
2022-02-26 16:58:03 +0100

    Player able to move with arrow keys (exercise 1-6)

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
+using DIKUArcade.GUI;
+using DIKUArcade;
 using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
@@ -5,26 +10,69 @@ using DIKUArcade.Math;
 
 namespace Galaga
 {
-    public class Game
+    public class Game : DIKUGame, IGameEventProcessor
     {
         private Player player;
-
-        public Game() {
+        private GameEventBus eventBus;
+        
+        public Game(WindowArgs windowArgs) : base(windowArgs){
             player = new Player(
             new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
             new Image(Path.Combine("Assets", "Images", "Player.png")));
+            eventBus = new GameEventBus();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+            window.SetKeyEventHandler(KeyHandler);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
         }
 
-        //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
+        private void KeyHandler(KeyboardAction action, KeyboardKey key){
+            //Console.WriteLine($"TestKeyEvents.KeyHandler({action}, {key})");
+            switch (action){
+                case KeyboardAction.KeyRelease:
+                    switch (key){
+                        case KeyboardKey.Escape:
+                            window.CloseWindow();
+                            break;
+                        case KeyboardKey.Left:
+                            player.SetMoveLeft(false);
+                            break;
+                        case KeyboardKey.Right:
+                            player.SetMoveRight(false);
+                            break;
+                    }
+                    break;
+                case KeyboardAction.KeyPress:
+                    switch (key){
+                        case KeyboardKey.Left:
+                            player.SetMoveLeft(true);
+                            break;
+                        case KeyboardKey.Right:
+                            player.SetMoveRight(true);
+                            break;
+                    }
+                    break;
+            }
+        }
 
-        // public override void Render()
-        // {
-        //     throw new System.NotImplementedException("Galaga game has nothing to render yet.");
-        // }
+        public override void Render(){
+            player.Render();
+        }
 
-        // public override void Update()
-        // {
-        //     throw new System.NotImplementedException("Galaga game has no entities to update yet.");
-        // }
+        public override void Update(){
+          eventBus.ProcessEventsSequentially();
+          player.Move();
+        }
+
+        public void ProcessEvent(GameEvent gameEvent){
+            if (gameEvent.EventType == GameEventType.WindowEvent){
+                switch (gameEvent.Message){
+                    // TODO: Implement
+                    default:
+                        break;
+                }
+            }
+        }
+
     }
+
 }
--- Galaga/Player.cs
+++ Galaga/Player.cs
@@ -7,14 +7,44 @@ namespace Galaga {
 
         private Entity entity;
         private DynamicShape shape;
+        private float moveLeft = 0.0f;
+        private float moveRight = 0.0f;
+        const float MOVEMENT_SPEED = 0.01f;
 
         public Player(DynamicShape shape, IBaseImage image) {
             entity = new Entity(shape, image);
             this.shape = shape;
         }
-        
+
         public void Render(){
-            // TODO: render the player entity
+            entity.RenderEntity();
+        }
+
+        public void Move(){
+            this.shape.Move();
+        }
+
+        private void UpdateMovement(){
+            this.shape.Direction.X = moveLeft + moveRight;
+        }
+
+        public void SetMoveLeft(bool val){
+            if (val){
+                this.moveLeft = -MOVEMENT_SPEED;
+            }
+            else{
+                this.moveLeft = 0;
+            }
+            UpdateMovement();
+        }
+        public void SetMoveRight(bool val){
+            if (val){
+                this.moveRight = +MOVEMENT_SPEED;
+            }
+            else{
+                this.moveRight = 0;
+            }
+            UpdateMovement();
         }
 
     }
