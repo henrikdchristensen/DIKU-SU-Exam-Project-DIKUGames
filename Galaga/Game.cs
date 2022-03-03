89d4506cba79026fa35ca5cd773fa3bacc96bbeb
Mads Jensen <mads.v.jensen@gmail.com>
2022-03-03 14:00:07 +0100

    Events are send on eventbus

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,3 +1,4 @@
+using System.Security.Principal;
 using System.Collections.Generic;
 using DIKUArcade.Events;
 using DIKUArcade.Input;
@@ -18,15 +19,22 @@ namespace Galaga
         private EntityContainer<Enemy> enemies;
         private EntityContainer<PlayerShot> playerShots;
         private IBaseImage playerShotImage;
-        
+        private AnimationContainer enemyExplosions;
+        private List<Image> explosionStrides;
+        private const int EXPLOSION_LENGTH_MS = 500;
+
         public Game(WindowArgs windowArgs) : base(windowArgs){
             player = new Player(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
+
             eventBus = new GameEventBus();
             eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
-            eventBus.Subscribe(GameEventType.InputEvent, this);
             window.SetKeyEventHandler(KeyHandler);
+            
+            eventBus.Subscribe(GameEventType.InputEvent, this);            
+            
+
             var images = ImageStride.CreateStrides(4, Path.Combine("Assets", "Images", "BlueMonster.png"));
             const int numEnemies = 8;
             enemies = new EntityContainer<Enemy>(numEnemies);
@@ -37,11 +45,23 @@ namespace Galaga
             }
             playerShots = new EntityContainer<PlayerShot>();
             playerShotImage = new Image(Path.Combine("Assets", "Images", "BulletRed2.png"));
+
+
+            enemyExplosions = new AnimationContainer(numEnemies);
+            explosionStrides = ImageStride.CreateStrides(8,
+                Path.Combine("Assets", "Images", "Explosion.png"));
+
+        }
+
+        public void AddExplosion(Vec2F position, Vec2F extent) {
+            enemyExplosions.AddAnimation(
+                new StationaryShape(position, extent), 
+                EXPLOSION_LENGTH_MS, 
+                new ImageStride(EXPLOSION_LENGTH_MS / 8, explosionStrides));            
         }
 
         private void IterateShots(){
             playerShots.Iterate(shot =>{
-                // TODO: move the shot's shape
                 shot.Shape.Move();
                 if (shot.Shape.Position.Y > 1){
                     shot.DeleteEntity();
@@ -52,48 +72,18 @@ namespace Galaga
                         if (data.Collision) {
                             shot.DeleteEntity();
                             enemy.DeleteEntity();
+                            AddExplosion(enemy.Shape.Position, enemy.Shape.Extent);
                         }
                     });
                 }
             });
         }
 
-        private void KeyHandler(KeyboardAction action, KeyboardKey key){
-            //Console.WriteLine($"TestKeyEvents.KeyHandler({action}, {key})");
-            switch (action){
-                case KeyboardAction.KeyRelease:
-                    switch (key){
-                        case KeyboardKey.Escape:
-                            window.CloseWindow();
-                            break;
-                        case KeyboardKey.Left:
-                            player.SetMoveLeft(false);
-                            break;
-                        case KeyboardKey.Right:
-                            player.SetMoveRight(false);
-                            break;
-                        case KeyboardKey.Space:
-                            playerShots.AddEntity(new PlayerShot(player.GetPosition(), playerShotImage));
-                            break;
-                    }
-                    break;
-                case KeyboardAction.KeyPress:
-                    switch (key){
-                        case KeyboardKey.Left:
-                            player.SetMoveLeft(true);
-                            break;
-                        case KeyboardKey.Right:
-                            player.SetMoveRight(true);
-                            break;
-                    }
-                    break;
-            }
-        }
-
         public override void Render(){
             player.Render();
             enemies.RenderEntities();
             playerShots.RenderEntities();
+            enemyExplosions.RenderAnimations();
         }
 
         public override void Update(){
@@ -101,12 +91,51 @@ namespace Galaga
           player.Move();
           IterateShots();
         }
+        
+        private void KeyHandler(KeyboardAction action, KeyboardKey key){
+            var e = new GameEvent {
+                Message = action.ToString(),
+                ObjectArg1 = key,
+                EventType = GameEventType.InputEvent
+            };
+            eventBus.RegisterEvent(e);
+        }
+
+        public void KeyPress(KeyboardKey key) {
+            switch (key){
+                case KeyboardKey.Escape:
+                    window.CloseWindow();
+                    break;
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(true);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(true);
+                    break;
+                case KeyboardKey.Space:
+                    playerShots.AddEntity(new PlayerShot(player.GetPosition(), playerShotImage));
+                    break;
+            }
+        }
+        public void KeyRelease(KeyboardKey key) {
+            switch (key){
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(false);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(false);
+                    break;
+            }
+        }
 
         public void ProcessEvent(GameEvent gameEvent){
-            if (gameEvent.EventType == GameEventType.WindowEvent){
+            if (gameEvent.EventType == GameEventType.InputEvent){
                 switch (gameEvent.Message){
-                    // TODO: Implement
-                    default:
+                    case "KeyPress":
+                        KeyPress((KeyboardKey)gameEvent.ObjectArg1);
+                        break;
+                    case "KeyRelease":
+                        KeyRelease((KeyboardKey)gameEvent.ObjectArg1);
                         break;
                 }
             }
