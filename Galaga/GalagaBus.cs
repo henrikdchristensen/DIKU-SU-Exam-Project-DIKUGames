28718b820c35985f4a3b0a8ff31ef00bfe9de7d2
Mads Jensen <2mads.v.jensen@gmail.com>
2022-03-24 13:26:27 +0100

    Test finished

--- Galaga/GalagaBus.cs
+++ Galaga/GalagaBus.cs
@@ -1,8 +1,11 @@
 using DIKUArcade.Events;
-namespace Galaga; 
-public static class GalagaBus {
-    private static GameEventBus eventBus;
-    public static GameEventBus GetBus() {
-        return GalagaBus.eventBus ?? (GalagaBus.eventBus = new GameEventBus());
+
+namespace Galaga {
+    public static class GalagaBus {
+        private static GameEventBus eventBus;
+        public static GameEventBus GetBus() {
+            return GalagaBus.eventBus ?? (GalagaBus.eventBus = new GameEventBus());
+        }
     }
 }
+
--- GalagaTests/TestSquadron.cs
+++ GalagaTests/TestSquadron.cs
@@ -16,7 +16,7 @@ namespace GalagaTests {
     [TestFixture]
     public class TestSquadron {
 
-        private const float COMPARE_DIFF = 10e-6f;
+        private const float COMPARE_DIFF = 10e-5f;
 
         [SetUp]
         public void InitializeTest() {
@@ -25,13 +25,11 @@ namespace GalagaTests {
 
         [Test]
         public void TestZigZag() { //TODO
-        }
+            List<Vec2F> expected = new List<Vec2F> { new Vec2F(0.3f, 0.9f), new Vec2F(0.4f, 0.8f), new Vec2F(0.5f, 0.9f), new Vec2F(0.6f, 0.8f), new Vec2F(0.7f, 0.9f) };
 
-        [Test]
-        public void TestVFormation() { //TODO
-            float speed = 0.005f;
             int enemyCount = 5;
-            ISquadron squadron = new VFormation(enemyCount, speed);
+            float speed = 0.005f;
+            ISquadron squadron = new Zigzag(enemyCount, speed);
             float startOffset = (squadron.MaxEnemies - enemyCount) / 2.0f * 0.1f;
 
             var enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("..", "Galaga", "Assets", "Images", "BlueMonster.png"));
@@ -40,13 +38,32 @@ namespace GalagaTests {
 
             squadron.CreateEnemies(enemyStridesBlue, enemyStridesRed);
 
-            int i = 1;
+            int i = 0;
             foreach (Enemy enemy in squadron.Enemies) {
-                float xPos = (float) i++ * 0.1f + startOffset;
-                float yPos = 0.9f;
-                Vec2F expectedPos = new Vec2F(xPos, yPos);
+                Vec2F expectedPos = expected[i++];
                 Vec2F diff = expectedPos - enemy.Shape.Position;
-                Assert.True(Math.Abs(diff.X) < COMPARE_DIFF && Math.Abs(diff.Y) < COMPARE_DIFF, $"Expected {diff.X} Actual pos {diff.Y}");
+                Assert.True(Math.Abs(diff.X) < COMPARE_DIFF && Math.Abs(diff.Y) < COMPARE_DIFF);
+            }
+        }
+
+        [Test]
+        public void TestVFormation() { //TODO
+            List<Vec2F> expected = new List<Vec2F> { new Vec2F(0.3f, 0.9f), new Vec2F(0.4f, 0.8f), new Vec2F(0.5f, 0.7f), new Vec2F(0.6f, 0.8f), new Vec2F(0.7f, 0.9f) };
+
+            int enemyCount = 5;
+            float speed = 0.005f;
+            ISquadron squadron = new VFormation(enemyCount, speed);
+
+            var enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("..", "Galaga", "Assets", "Images", "BlueMonster.png"));
+            var enemyStridesRed = ImageStride.CreateStrides(2, Path.Combine("..", "Galaga", "Assets", "Images", "RedMonster.png"));
+
+            squadron.CreateEnemies(enemyStridesBlue, enemyStridesRed);
+            
+            int i = 0;
+            foreach (Enemy enemy in squadron.Enemies) {
+                Vec2F expectedPos = expected[i++];
+                Vec2F diff = expectedPos - enemy.Shape.Position;
+                Assert.True(Math.Abs(diff.X) < COMPARE_DIFF && Math.Abs(diff.Y) < COMPARE_DIFF, $"{diff}");
             }
         }
 
@@ -70,7 +87,7 @@ namespace GalagaTests {
                 float yPos = 0.9f;
                 Vec2F expectedPos = new Vec2F(xPos, yPos);
                 Vec2F diff = expectedPos - enemy.Shape.Position;
-                Assert.True(Math.Abs(diff.X) < COMPARE_DIFF && Math.Abs(diff.Y) < COMPARE_DIFF, $"Expected {diff.X} Actual pos {diff.Y}");
+                Assert.True(Math.Abs(diff.X) < COMPARE_DIFF && Math.Abs(diff.Y) < COMPARE_DIFF);
             }
 
         }
