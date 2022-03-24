d402f52910714cd9de18ffbe7f29e9e82202cd71
Mads Jensen <2mads.v.jensen@gmail.com>
2022-03-24 10:43:33 +0100

    aske

--- GalagaTests/TestMovementStrategy.cs
+++ GalagaTests/TestMovementStrategy.cs
@@ -11,6 +11,7 @@ using Galaga.MovementStrategy;
 using Galaga.Squadron;
 using Galaga.GalagaStates;
 using Galaga;
+using System.Linq;
 
 using DIKUArcade.Graphics;
 
@@ -19,18 +20,15 @@ namespace GalagaTests {
     [TestFixture]
     class TestMovementStrategy {
 
-        // props
-        private List<ISquadron> squadronList;
 
         private List<IMovementStrategy> movementStrategyList;
 
-        //private ISquadron straightFormation;
-        //private ISquadron zigzag;
-        //private ISquadron vformation;
+        private ISquadron straightFormation;
 
-        private EntityContainer<Enemy> enemies;
+        private Enemy enemy;
 
-        private float speed = 0.5f;
+        private float speed = 0.005f;
+        private float diff = 0.002f;
 
 
         [SetUp]
@@ -46,20 +44,21 @@ namespace GalagaTests {
             var enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("..", "Galaga", "Assets", "Images", "BlueMonster.png"));
             var enemyStridesRed = ImageStride.CreateStrides(2, Path.Combine("..", "Galaga", "Assets", "Images", "RedMonster.png"));
 
-            var straightFormation = new Straight(1, speed);
+            straightFormation = new Straight(1, speed);
             //zigzag = new Galaga.Squadron.Zigzag(9, 0.05f);
             //vformation = new VFormation(9, 0.05f);
 
             straightFormation.CreateEnemies(enemyStridesBlue, enemyStridesRed);
-            enemies = straightFormation.Enemies;
+            enemy = new Enemy()
+            //straightFormation.Enemies;
             // zigzag.CreateEnemies(enemyStridesBlue, enemyStridesRed);
             // vformation.CreateEnemies(enemyStridesBlue, enemyStridesRed);
 
-            squadronList = new List<ISquadron> {
-               new Straight(9, 0.05f),
-               new Galaga.Squadron.Zigzag(9, 0.05f),
-               new VFormation(9, 0.05f)
-            };
+            // squadronList = new List<ISquadron> {
+            //    new Straight(9, 0.05f),
+            //    new Galaga.Squadron.Zigzag(9, 0.05f),
+            //    new VFormation(9, 0.05f)
+            // };
 
         }
 
@@ -72,17 +71,37 @@ namespace GalagaTests {
 
         [Test]
         public void TestDown() { //Is enemy's position decremented with enemy.speed, when move strategy down is used
-            movementStrategyList[0].MoveEnemies(enemies);
-            foreach (Enemy e in enemies) {
-                Assert.That(e.InitialPos.Y - speed*10 == e.Shape.Position.Y, $"Old pos: {e.InitialPos.Y - speed* 10 } New pos {e.Shape.Position.Y}");
+            for (int i = 0; i < 10; i++) {
+                movementStrategyList[0].MoveEnemies(enemies);
             }
-
+            foreach (Enemy e in enemies) {
+                var expectedPos = e.InitialPos.Y - speed * 10;
+                Assert.True( expectedPos >= expectedPos-diff && expectedPos <= expectedPos+diff, $"Expected pos: {expectedPos} New pos {e.Shape.Position.Y}");
+            }
         }
 
         [Test]
         public void TestZigZag() { //Is enemy's position set correct when move strategy zisag is used
                                    //squadronList[0].CreateEnemies(enemyStridesBlue, enemyStridesRed);
 
+            const float PERIOD = 0.045f;
+            const float AMPLITUDE = 0.05f;
+            float expected_y_i;
+            float expected_x_i;
+
+            for (int i = 0; i < 10; i++) {
+                //var enemy = enemies.
+                movementStrategyList[2].MoveEnemy(enemies);
+                y_i = enemy[].Shape.Position.Y - enemy.Speed;
+                x_i = initialPos.X + (float)(AMPLITUDE * Math.Sin((2.0f * Math.PI * (initialPos.Y-y_i)) / PERIOD));
+            }
+
+            foreach (Enemy enemy in enemies) {
+                Assert.True( 
+                expected_x_i >= expected_x_i-diff && expected_x_i <= expected_x_i+diff &&
+                expected_y_i >= expected_y_i-diff && expected_y_i <= expected_y_i+diff
+                , $"Expected pos: {expected_x_i} New pos {e.Shape.Position.X} AND Expected pos: {expected_y_i} New pos {e.Shape.Position.Y}");
+            }
 
         }
 
