aa1f0be2ec9957acec9163036a6e7f4b672625f9
Mads Jensen <2mads.v.jensen@gmail.com>
2022-05-06 11:10:26 +0200

    Icollidable interface added

---
+++ Galaga/Galaga/Collision/Interface1.cs
@@ -0,0 +1,11 @@
+﻿using DIKUArcade.Entities;
+
+namespace Galaga.Collision {
+
+    interface ICollidable {
+
+        public Shape GetShape();
+
+        public void IsCollided(Shape other);
+    }
+}
