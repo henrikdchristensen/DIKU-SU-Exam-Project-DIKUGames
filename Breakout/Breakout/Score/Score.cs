a4f93b61397c2b4010a36086fe2f0c4174732de2
Henrik Christensen <henrikdchristensen@gmail.com>
2022-05-06 11:42:52 +0200

    added Score + test #4, #15

---
+++ Breakout/Breakout/Score/Score.cs
@@ -0,0 +1,37 @@
+﻿using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace Breakout.Score {
+    public class Score {
+        public int Points {
+            get; private set;
+        }
+        private Text display;
+
+        /// <summary> A scoreboard </summary>
+        /// <param name = "position"> Where the scoreboard is rendered</param>
+        /// <param name = "extent"> The extent (size) of the scoreboard </param>
+        /// <returns> A Score instance </returns>
+        public Score(Vec2F position, Vec2F extent) {
+            Points = 0;
+            display = new Text(Points.ToString(), position, extent);
+            display.SetColor(new Vec3F(1f, 1f, 1f));
+        }
+
+        /// <summary> Add one point to the score </summary>
+        public void AddPoints() {
+            Points++;
+            display.SetText(Points.ToString());
+        }
+
+        /// <summary> Render the score on the screen </summary>
+        public void RenderScore() {
+            display.RenderText();
+        }
+
+        public void Reset() {
+            Points = 0;
+            display.SetText(Points.ToString());
+        }
+    }
+}
---
+++ Breakout/BreakoutTests/TestScore.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using DIKUArcade.GUI;
+using DIKUArcade.Math;
+using Breakout.Score;
+
+namespace BreakoutTests {
+
+    [TestFixture]
+    public class TestScore {
+        private Score score;
+
+        [SetUp]
+        public void InitializeTest() {
+            Window.CreateOpenGLContext();
+            score = new Score(new Vec2F(0, 0), new Vec2F(0, 0));
+        }
+
+        [Test]
+        public void TestAddPoints() {
+            int oldPoints = score.Points;
+            score.AddPoints();
+            Assert.True(score.Points > oldPoints);
+        }
+    }
+}
