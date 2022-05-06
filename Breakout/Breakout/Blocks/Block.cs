87f05671acbf46978c60ec23e843ea51dd15b815
a-lundsgaard <askelundsgaard@live.dk>
2022-05-06 15:03:01 +0200

    new block

--- Breakout/Breakout/Blocks/Block.cs
+++ Breakout/Breakout/Blocks/Block.cs
@@ -5,19 +5,20 @@ using Breakout.Collision;
 namespace Breakout.Items {
 
     public class Block : Entity, IItem, ICollidable {
-        public int Health {
-            get; set;
-        } = 10;
 
-        private int Threshold { get; set; } = 10;
+
+        public int StartHealt { get; set; } = 10;
+
+        public int Health { get; set; }
 
         public Block(StationaryShape shape, IBaseImage image) : base(shape, image) {
             CollisionHandler.GetInstance().Subsribe(this);
+            Health = StartHealt;
         }
 
         /// <summary> Should be called when the block is hit, and decrements health </summary>
         /// <returns> Returns true if it is dead, and false otherwise </returns>
-        public bool Hit() {
+        virtual public bool Hit() {
             Health--;
             if (Health < 0)
                 return true;
--- Breakout/Breakout/Blocks/HardenedBlock.cs
+++ Breakout/Breakout/Blocks/HardenedBlock.cs
@@ -4,10 +4,21 @@ using Breakout.Items;
 
 public class HardenedBlock : Block {
 
-    public HardenedBlock(StationaryShape shape, IBaseImage image) : base(shape, image) {
-        Health = Health*2;
-        
+    private IBaseImage blockStridesAlt;
+
+    public HardenedBlock(StationaryShape shape, IBaseImage image, IBaseImage blockStridesAlt) : base(shape, image) {
+        StartHealt = base.StartHealt * 2;
+        this.blockStridesAlt = blockStridesAlt;
     }
 
+    override public bool Hit() {
+        Health--;
+        if (Health < 0)
+            return true;
+        else if (Health < StartHealt / 2) {
+            Image = blockStridesAlt;
+        }
+        return false;
+    }
 
 }
