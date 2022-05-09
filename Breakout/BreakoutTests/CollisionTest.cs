cfac142bb303a538e6b74bd2ef71fcdbf4733c8b
Henrik Christensen <henrikdchristensen@gmail.com>
2022-05-09 17:35:21 +0200

    added file for collision test

---
+++ Breakout/BreakoutTests/CollisionTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using Breakout.Collision;
+
+namespace BreakoutTests {
+
+    [TestFixture]
+    public class CollisionTest {
+        
+        [SetUp]
+        public void InitiateTest() {
+            Window.CreateOpenGLContext();
+        }
+
+        [Test]
+        public void Test1() {
+        }
+
+    }
+}
