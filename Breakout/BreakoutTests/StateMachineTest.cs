859e669a563b68f916488cf4b0588bda1676aa4b
Henrik Christensen <henrikdchristensen@gmail.com>
2022-05-09 17:30:15 +0200

    added state machine test

---
+++ Breakout/BreakoutTests/StateMachineTest.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using Breakout.Game;
+using Breakout.Game.States;
+
+namespace BreakoutTests {
+
+    [TestFixture]
+    public class StateMachineTest {
+        private StateMachine stateMachine;
+        private GameEventBus eventBus;
+
+        [SetUp]
+        public void InitiateStateMachine() {
+            Window.CreateOpenGLContext();
+            stateMachine = new StateMachine();
+            eventBus = new GameEventBus();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.GameStateEvent });
+            eventBus.Subscribe(GameEventType.GameStateEvent, stateMachine);
+        }
+
+        [Test]
+        public void TestInitialState() {
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<MainMenu>());
+        }
+
+        [Test]
+        public void TestEventGamePaused() {
+            eventBus.RegisterEvent(
+                new GameEvent {
+                    EventType = GameEventType.GameStateEvent,
+                    Message = "CHANGE_STATE",
+                    StringArg1 = "GAME_PAUSED"
+                }
+            );
+            eventBus.ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GamePaused>());
+        }
+
+        [Test]
+        public void TestEventGameRunning() {
+            eventBus.RegisterEvent(
+                new GameEvent {
+                    EventType = GameEventType.GameStateEvent,
+                    Message = "CHANGE_STATE",
+                    StringArg1 = "GAME_RUNNING"
+                }
+            );
+            eventBus.ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<GameRunning>());
+        }
+
+        [Test]
+        public void TestEventMainMenu() {
+            eventBus.RegisterEvent(
+                new GameEvent {
+                    EventType = GameEventType.GameStateEvent,
+                    Message = "CHANGE_STATE",
+                    StringArg1 = "MAIN_MENU"
+                }
+            );
+            eventBus.ProcessEventsSequentially();
+            Assert.That(stateMachine.ActiveState, Is.InstanceOf<MainMenu>());
+        }
+    }
+}
