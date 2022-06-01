e0523797a673d2566f9a80b4c149b0a5b54cf6d6
a-lundsgaard <askelundsgaard@live.dk>
2022-06-01 20:57:55 +0200

    test

--- Breakout/BreakoutTests/TestLogger.cs
+++ Breakout/BreakoutTests/TestLogger.cs
@@ -5,8 +5,8 @@ class TestLogger {
     /// <summary>
     /// Write out expected result and actual result
     /// </summary>
+    /// <returns>string</returns>    
     public static string OnFailedTestMessage<T>(T expected, T result) {
         return $"Expected: {expected}, but got {result}";
     }
-
 }
