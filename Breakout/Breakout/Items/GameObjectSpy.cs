9ca985f2f7a6e20085cdb2131df43584602871e7
Henrik Christensen <henrikdchristensen@gmail.com>
2022-06-01 15:52:39 +0200

    added test spy which checks whether or not an GameObject class has been called + added comments to CollisionHandlerTests

---
+++ Breakout/Breakout/Items/GameObjectSpy.cs
@@ -0,0 +1,19 @@
+﻿using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Physics;
+
+namespace Breakout.Items {
+
+    public class GameObjectSpy : GameObject {
+
+        public bool hasCollided {
+            get; private set;
+        }
+
+        public GameObjectSpy(Shape shape, IBaseImage image) : base(shape, image) { }
+
+        public override void Accept(GameObject other, CollisionData data) {
+            hasCollided = true;
+        }
+    }
+}
--- Breakout/BreakoutTests/CollisionHandlerTest.cs
+++ Breakout/BreakoutTests/CollisionHandlerTest.cs
@@ -1,20 +1,15 @@
 using NUnit.Framework;
-using System.Collections.Generic;
-using DIKUArcade.GUI;
-using DIKUArcade.Events;
-using Breakout.Collision;
-using Breakout.Items;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
-using Breakout.Items;
-using Breakout;
 using DIKUArcade.Math;
-using System;
+using Breakout;
+using Breakout.Collision;
+using Breakout.Items;
 
 namespace BreakoutTests {
 
     [TestFixture]
-    public class CollisionTest {
+    public class CollisionHandlerTest {
 
         private CollisionHandler collision = CollisionHandler.GetInstance();
 
@@ -23,23 +18,31 @@ namespace BreakoutTests {
         /// </summary>
         [SetUp]
         public void InitiateTest() {
+            
         }
 
         /// <summary>
-        /// 
+        /// Test that collision between ball and block can happen.
         /// </summary>
         [Test]
         public void TestCollisionBallAndBlock() {
             DynamicShape shape = new DynamicShape(0.4f, 0.1f, 0.1f, 0.1f);
-            Ball ball = new Ball(shape, new NoImage());
-            shape.ChangeDirection(new Vec2F(0.0f, 1.0f));
-            Block block = new Block(new StationaryShape(0.4f, 0.5f, 0.1f, 0.1f), new NoImage());
+            Ball ball = new Ball(shape, new NoImage()); // pos(0.4,0.1), width(0.1,0.1)
+            Block block = new Block(new StationaryShape(0.4f, 0.5f, 0.1f, 0.1f), new NoImage()); // pos(0.4,0.5), width(0.1,0.1)
 
+            shape.ChangeDirection(new Vec2F(0.0f, 1.0f)); // move ball towards block for colliding
+            
             int oldHealth = block.Health;
             Vec2F oldDir = shape.Direction.Copy();
+
+            // Put ball and block in collision table
             collision.Subsribe(ball);
             collision.Subsribe(block);
+
+            // Update collisions for block loosing health, which can be checked for Assert()
             collision.Update();
+
+            // Check that block has lost health (collision has happened) and ball has changed direction
             Assert.True(oldHealth > block.Health);
             Assert.True(oldDir.X != shape.Direction.X || oldDir.Y != shape.Direction.Y);
         }
