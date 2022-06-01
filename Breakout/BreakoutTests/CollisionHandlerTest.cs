271d175c0f2d4b4d1d2b45849ce7ee3580c16a41
Henrik Christensen <henrikdchristensen@gmail.com>
2022-06-01 16:56:39 +0200

    added whitebox test for collisionHandler

--- Breakout/BreakoutTests/CollisionHandlerTest.cs
+++ Breakout/BreakoutTests/CollisionHandlerTest.cs
@@ -22,47 +22,86 @@ namespace BreakoutTests {
         }
 
         /// <summary>
-        /// Test that collision between ball and block can happen.
+        /// Integration test: Test that collision between ball and block can happen.
         /// </summary>
         [Test]
         public void TestCollisionBallAndBlock() {
             DynamicShape shape = new DynamicShape(0.4f, 0.1f, 0.1f, 0.1f);
-            Ball ball = new Ball(shape, new NoImage()); // pos(0.4,0.1), width(0.1,0.1)
-            Block block = new Block(new StationaryShape(0.4f, 0.5f, 0.1f, 0.1f), new NoImage()); // pos(0.4,0.5), width(0.1,0.1)
+            Ball ball = new Ball(shape, new NoImage()); // pos(0.4,0.1)
+            Block block = new Block(new StationaryShape(0.4f, 0.4f, 0.1f, 0.1f), new NoImage()); // pos(0.4,0.5)
 
-            shape.ChangeDirection(new Vec2F(0.0f, 1.0f)); // move ball towards block for colliding
-            
-            int oldHealth = block.Health;
+            // Store old direction and health for comparing
             Vec2F oldDir = shape.Direction.Copy();
+            int oldHealth = block.Health;
 
             // Put ball and block in collision table
             collision.Subsribe(ball);
             collision.Subsribe(block);
 
+            // Move ball towards block for colliding
+            shape.ChangeDirection(new Vec2F(0.0f, 1.0f));
+
             // Update collisions for block loosing health, which can be checked for Assert()
             collision.Update();
 
-            // Check that block has lost health (collision has happened) and ball has changed direction
+            // Check that block has lost health and ball has changed direction after colliding
             Assert.True(oldHealth > block.Health);
             Assert.True(oldDir.X != shape.Direction.X || oldDir.Y != shape.Direction.Y);
         }
 
         /// <summary>
-        /// 
+        /// Integration test: Test that collision between ball and player can happen.
         /// </summary>
         [Test]
         public void TestCollisionBallAndPlayer() {
-            DynamicShape shape = new DynamicShape(0.4f, 0.11f, 0.1f, 0.1f);
-            Ball ball = new Ball(shape, new NoImage());
-            shape.ChangeDirection(new Vec2F(0.0f, -0.3f));
-            Player player = new Player(new DynamicShape(0.4f, 0.01f, 0.1f, 0.1f), new NoImage());
+            DynamicShape shape = new DynamicShape(0.4f, 0.4f, 0.1f, 0.1f);
+            Ball ball = new Ball(shape, new NoImage()); // pos(0.4,0.4)
+            Player player = new Player(new DynamicShape(0.4f, 0.1f, 0.1f, 0.1f), new NoImage()); // pos(0.4,0.1)
 
+            // Store old direction
             Vec2F oldDir = shape.Direction.Copy();
+
+            // Put ball and block in collision table
             collision.Subsribe(ball);
             collision.Subsribe(player);
-            collision.Update();
-            Assert.True(oldDir.X != shape.Direction.X || oldDir.Y != shape.Direction.Y);
 
+            shape.ChangeDirection(new Vec2F(0.0f, -1.0f)); // move ball towards player for colliding
+
+            // Update collisions for block loosing health, which can be checked for Assert()
+            collision.Update();
+
+            // Check that ball has changed direction after colliding
+            Assert.True(oldDir.X != shape.Direction.X || oldDir.Y != shape.Direction.Y);
+        }
+
+        /// <summary>
+        /// Whitebox test: 100% Statement and Branch coverage:
+        /// Test both that collision can happen and not depending on given direction.
+        /// </summary>
+        [TestCase(0.0f, false)] // Only B1 executed
+        [TestCase(1.0f, true)] // Both B1 & B1 executed (100% statement coverage)
+        // Both test cases ensures 100% branch coverage
+        public void TestUpdate(float dir, bool expected) {
+            DynamicShape shape = new DynamicShape(0.0f, 0.0f, 0.1f, 0.1f);
+            GameObjectSpy obj1 = new GameObjectSpy(shape, new NoImage());
+            GameObjectSpy obj2 = new GameObjectSpy(new StationaryShape(0.0f, 1.0f, 0.1f, 0.1f), new NoImage());
+
+            // Store old hasCollided (should here be false)
+            bool objOldHasCollided = obj1.hasCollided || obj2.hasCollided;
+
+            // Put shapes in collision table
+            collision.Subsribe(obj1);
+            collision.Subsribe(obj2);
+
+            // Move shape towards other shape for collision
+            shape.ChangeDirection(new Vec2F(0.0f, dir));
+
+            // Update collisions for shapes for checking that Accept method has been called
+            collision.Update();
+
+            // Check that oldHasCollided is false and
+            Assert.True(objOldHasCollided == false &&
+                obj1.hasCollided == expected && obj2.hasCollided == expected);
         }
 
     }
