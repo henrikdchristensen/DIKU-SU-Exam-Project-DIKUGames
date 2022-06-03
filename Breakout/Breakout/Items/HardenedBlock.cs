9c184e37223e58069fea19dc56fdab95cffcef6e
Henrik Christensen <henrikdchristensen@gmail.com>
2022-06-03 13:41:27 +0200

    set hardened and unbreakable under namespace

--- Breakout/Breakout/Items/HardenedBlock.cs
+++ Breakout/Breakout/Items/HardenedBlock.cs
@@ -2,34 +2,38 @@ using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using Breakout.Items;
 
-public class HardenedBlock : Block {
+namespace Breakout.Items {
 
-    private IBaseImage blockStridesAlt;
+    public class HardenedBlock : Block {
 
-    /// <summary>
-    /// Constructor for HardenedBlock: StartHealth is 2 times higher than normal block,
-    /// also point reward is 2 times higher than normal
-    /// </summary>
-    /// <param name="shape">StationaryShape of the HardenedBlock</param>
-    /// <param name="image">Image of the HardenedBlock</param>
-    /// <param name="blockStridesAlt">Image of the HardenedBlock after hits</param>
-    public HardenedBlock(StationaryShape shape, IBaseImage image, IBaseImage blockStridesAlt) : base(shape, image) {
-        StartHealt *= 2;
-        PointReward *= 2;
-        this.blockStridesAlt = blockStridesAlt;
-    }
+        private IBaseImage blockStridesAlt;
 
-    /// <summary>
-    /// Reduce health of block and change image if #hits has occured.
-    /// If under zero then delete block.
-    /// </summary>
-    override public void Hit() {
-        Console.WriteLine("Hardened hit");
-        Health--;
-        if (Health < 0)
-            DeleteEntity();
-        else if (Health < StartHealt / 2)
-            Image = blockStridesAlt;
+        /// <summary>
+        /// Constructor for HardenedBlock: StartHealth is 2 times higher than normal block,
+        /// also point reward is 2 times higher than normal
+        /// </summary>
+        /// <param name="shape">StationaryShape of the HardenedBlock</param>
+        /// <param name="image">Image of the HardenedBlock</param>
+        /// <param name="blockStridesAlt">Image of the HardenedBlock after hits</param>
+        public HardenedBlock(StationaryShape shape, IBaseImage image, IBaseImage blockStridesAlt) : base(shape, image) {
+            StartHealt *= 2;
+            PointReward *= 2;
+            this.blockStridesAlt = blockStridesAlt;
+        }
+
+        /// <summary>
+        /// Reduce health of block and change image if #hits has occured.
+        /// If under zero then delete block.
+        /// </summary>
+        override public void Hit() {
+            Console.WriteLine("Hardened hit");
+            Health--;
+            if (Health < 0)
+                DeleteEntity();
+            else if (Health < StartHealt / 2)
+                Image = blockStridesAlt;
+        }
+
     }
 
 }
--- Breakout/Breakout/Items/UnbreakAble.cs
+++ Breakout/Breakout/Items/UnbreakAble.cs
@@ -4,20 +4,24 @@ using DIKUArcade.Physics;
 using Breakout.Items;
 using Breakout.Collision;
 
-public class Unbreakable : Block {
+namespace Breakout.Items {
 
-    /// <summary>Constructor of Unbreakable: Setup that it can't be destroyed</summary>
-    /// <param name="shape">StationaryShape of the Unbreakable block</param>
-    /// <param name="image">Image of the Unbreakable block</param>
-    public Unbreakable(StationaryShape shape, IBaseImage image) : base(shape, image) {
-        IsDestroyable = false;
-    }
+    public class Unbreakable : Block {
 
-    /// <summary>Accepts another GameObject if collision has occured with another object.</summary>
-    /// <param name="other">The other GameObject</param>
-    /// <param name="data">Collision data</param>
-    public override void Accept(GameObject other, CollisionHandlerData data) {
-        other.UnbreakableCollision(data);
+        /// <summary>Constructor of Unbreakable: Setup that it can't be destroyed</summary>
+        /// <param name="shape">StationaryShape of the Unbreakable block</param>
+        /// <param name="image">Image of the Unbreakable block</param>
+        public Unbreakable(StationaryShape shape, IBaseImage image) : base(shape, image) {
+            IsDestroyable = false;
+        }
+
+        /// <summary>Accepts another GameObject if collision has occured with another object.</summary>
+        /// <param name="other">The other GameObject</param>
+        /// <param name="data">Collision data</param>
+        public override void Accept(GameObject other, CollisionHandlerData data) {
+            other.UnbreakableCollision(data);
+        }
+
     }
 
 }
