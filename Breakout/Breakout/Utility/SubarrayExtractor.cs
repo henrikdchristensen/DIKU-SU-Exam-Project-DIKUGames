22b8e6485b0bada22a957599c8ab212c08776cf7
a-lundsgaard <askelundsgaard@live.dk>
2022-06-08 10:32:14 +0200

    xml

--- Breakout/Breakout/Utility/SubarrayExtractor.cs
+++ Breakout/Breakout/Utility/SubarrayExtractor.cs
@@ -3,12 +3,12 @@
     public class SubarrayExtractor {
 
         /// <summary>
-        /// TODO
+        /// Static method for extracting for sub arrays between start and end keyword
         /// </summary>
-        /// <param name="arr">TODO</param>
-        /// <param name="startKeyword">TODO</param>
-        /// <param name="endKeyword">TODO</param>
-        /// <returns>TODO</returns>
+        /// <param name="arr">String array subrtact strings from</param>
+        /// <param name="startKeyword">Start word</param>
+        /// <param name="endKeyword">End word</param>
+        /// <returns>New subarray with the extracted lines from start to end keyword</returns>
         public static string[] Extract(string[] arr, string startKeyword, string endKeyword) {
             List<string> lst = new List<string>();
             bool startExtract = false;
